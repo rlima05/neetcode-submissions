478f418f81d6724fbaa78ee1bc295d95708f7076
Raphael Lima Fernandes <raphael.lima05@gmail.com>
2026-07-27 17:07:00 +0100

    Add: longest-palindromic-substring - submission-2

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-2.cs
@@ -0,0 +1,41 @@
+public class Solution {
+    public string LongestPalindrome(string s) {
+        int resLen = 0, resIdx = 0;
+
+        for(int i = 0; i < s.Length; i++)
+        {
+            int l = i, r = i;
+
+            while(l >= 0 && r < s.Length && s[r] == s[l])
+            {
+                if(r - l + 1 > resLen)
+                {
+                    resIdx = l;
+                    resLen = r - l +1;
+                }
+                l--;
+                r++;
+            }
+
+            l = i;
+            r = i+1;
+            while(l >= 0 && r < s.Length && s[r] == s[l])
+            {
+                if(r - l + 1 > resLen)
+                {
+                    resIdx = l;
+                    resLen = r - l +1;
+                }
+                l--;
+                r++;
+            }
+        }
+
+        return s.Substring(resIdx, resLen);
+    }
+}
+
+/*
+
+
+*/
