45317c52d61ece25dd66347b2b8cbbe951f7d665
Raphael Lima Fernandes <raphael.lima05@gmail.com>
2026-08-09 19:04:01 +0100

    Add: trapping-rain-water - submission-1

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-1.cs
@@ -0,0 +1,29 @@
+public class Solution {
+    public int Trap(int[] height) {
+        if(height.Length == 0) return 0;
+
+        int l = 0;
+        int r = height.Length - 1;
+        int leftMax = height[l];
+        int rightMax = height[r];
+        int res = 0;
+
+        while(l < r)
+        {
+            if(height[l] < height[r])
+            {
+                l++;
+                leftMax = Math.Max(leftMax, height[l]);
+                res+= leftMax - height[l];
+            }
+            else
+            {
+                r--;
+                rightMax = Math.Max(rightMax, height[r]);
+                res+= rightMax - height[r];
+            }
+        }
+
+        return res;
+    }
+}
